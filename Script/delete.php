08515f126f643fe1a2bd49a056b43f62555d9526
Zamasu <chinnawat.sinnarai@gmail.com>
2016-05-27 19:54:05 +0700

    3

--- Script/delete.php
+++
@@ -1,8 +0,0 @@
-<?
-session_start();
-$link = mysqli_connect("localhost", "it57160171", "0840799660za", "it57160171");
-$ID = mysqli_real_escape_string($link, $_POST['id']);
-$sql = "DELETE FROM `it57160171`.`ProjEnd_text` WHERE `ProjEnd_text`.`ID` = '".$ID."'" ;
-$query = mysqli_query($link, $queryString);
-
-?>
